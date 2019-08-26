e96c901e526bb1f7325a6d3de36c98db04f490e3
lwnica <nanami.nica@gmail.com>
2019-08-26 23:18:17 +0900

    google drive からリソース取ってくる

--- get_resources.sh
+++ get_resources.sh
@@ -2,6 +2,12 @@
 RESOUCES_DIR="./resources"
 CASCADE_DIR="${RESOUCES_DIR}/cv2_cascade"
 MODELS_DIR="${RESOUCES_DIR}/models"
+GOOGLE_DRIVE_ENDPOINT="https://drive.google.com/uc?export=download&id="
+
+if [ -z "${CLASSES_URL}" -o -z "${DENSENET_URL}" -o -z "${RESNET_URL}" ] ; then
+    echo "environment variables CLASSES_URL or DENSENET_URL or RESNET_URL missing"
+    exit 1
+fi
 
 if [ ! -e "${CASCADE_DIR}" ] ; then
     mkdir -p "${CASCADE_DIR}"
@@ -13,3 +19,9 @@ if [ ! -e "${MODELS_DIR}" ] ; then
     mkdir -p "${MODELS_DIR}"
 fi
 
+# download classes
+curl -kL "${GOOGLE_DRIVE_ENDPOINT}${CLASSES_URL}" -o "${MODELS_DIR}/idol_recognition_classes.txt"
+# download densenet model
+curl -kL "${GOOGLE_DRIVE_ENDPOINT}${DENSENET_URL}" -o "${MODELS_DIR}/idol_recognition_densenet.model"
+# download resnet model
+curl -kL "${GOOGLE_DRIVE_ENDPOINT}${RESNET_URL}" -o "${MODELS_DIR}/idol_recognition_resnet.model"
